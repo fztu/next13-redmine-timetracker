3f620a149408bb23ff848d110de68c1bd454fba1
Fazhi Tu <fazhi.tu@gmail.com>
2023-09-04 15:49:52 -0700

    feat: setup dashboard and landing page routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return ( 
+        <div>
+            Dashbard Page (Protected)
+        </div>
+     );
+}
+ 
+export default DashboardPage;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <p className='text6-xl text-green-500'>The easiest way to log your time in Redmine!</p>
-  )
-}
