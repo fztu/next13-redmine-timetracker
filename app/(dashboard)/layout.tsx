0bde45c42e111aa6cc6f1404b3c6b951bad7d37c
Fazhi Tu <fazhi.tu@gmail.com>
2023-09-04 20:53:56 -0700

    feat: sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 const DashboardLayout = ({
     children
@@ -10,9 +11,7 @@ const DashboardLayout = ({
     return ( 
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <div>
-                    Hello Sidebar
-                </div>
+                <Sidebar />
             </div>
             <main className="md:pl-72">
                 <Navbar />
